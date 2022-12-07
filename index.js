81b65d52eb8f9931f72be031f96adda47ec6ab73
gilito <elekasito@gmail.com>
2022-12-07 17:33:16 +0100

    Primer commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const lista = [
+    "Santi",
+    27,
+    true,
+    new Date("14 July 1995"),
+    {
+        titulo: "Harry Potter y la piedra filosofal",
+        autor : "J.K Rowling",
+        fecha: new Date("1 June 1997"),
+        url : new URL("https://es.wikipedia.org/wiki/Harry_Potter_y_la_piedra_filosofal")
+    } 
+
+]
